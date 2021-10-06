df550aa8b96a392271202353d118a4d30ec3e21c
Felipe Lin <felipe.lin2018@hotmail.com>
2021-10-06 18:56:11 +1100

    Redesigned loading component.

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Loading = () => {
   return (
     <div className="loading loading__container">
-      <p>Loading Blog Posts...</p>
+      <FontAwesomeIcon icon={faSpinner} size="4x" />
+      <p className=" loading__content">
+        Loading Blog Posts... If posts are not loaded, your session might have
+        expired. Please login and try again.
+      </p>
     </div>
   );
 };
